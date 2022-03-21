07fa907f1ddffca8902ae208b3a8d472a371a035
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-03-22 02:08:08 +0800

    chore(linux-aarch64): latest solc versions in list and commit bump

--- src/releases.rs
+++ src/releases.rs
@@ -25,7 +25,7 @@ static OLD_SOLC_RELEASES: Lazy<Releases> = Lazy::new(|| {
 });
 
 static LINUX_AARCH64_URL_PREFIX: &str =
-    "https://github.com/nikitastupin/solc/raw/3890b86a62fe6b8efd2f643f4adcd854f478b623/linux/aarch64";
+    "https://github.com/nikitastupin/solc/raw/08e633c3585e2a053f0662714098440e639a074a/linux/aarch64";
 
 static LINUX_AARCH64_RELEASES: Lazy<Releases> = Lazy::new(|| {
     serde_json::from_str(include_str!("../list/linux-aarch64.json"))
