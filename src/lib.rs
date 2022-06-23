84687f2eccbf0c5633f8e33ec15f9d0830b9587b
Rohit Narurkar <rohit.narurkar@protonmail.com>
2022-06-23 10:12:51 +0200

    chore: refactor version req check for NixOS patching

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use once_cell::sync::Lazy;
-use semver::Version;
+use semver::{Version, VersionReq};
 use sha2::Digest;
 
 use std::{
@@ -44,6 +44,9 @@ pub static SVM_HOME: Lazy<PathBuf> = Lazy::new(|| {
 /// The timeout to use for requests to the source
 const REQUEST_TIMEOUT: Duration = Duration::from_secs(60);
 
+/// Version beyond which solc binaries are not fully static, hence need to be patched for NixOS.
+static NIXOS_PATCH_REQ: Lazy<VersionReq> = Lazy::new(|| VersionReq::parse(">=0.8.0").unwrap());
+
 // Installer type that copies binary data to the appropriate solc binary file:
 // 1. create target file to copy binary data
 // 2. copy data
@@ -69,7 +72,7 @@ impl Installer {
         let mut content = Cursor::new(&self.binbytes);
         std::io::copy(&mut content, &mut f)?;
 
-        if platform::is_nixos() && self.version.major == 0 && self.version.minor >= 8 {
+        if platform::is_nixos() && NIXOS_PATCH_REQ.matches(&self.version) {
             patch_for_nixos(solc_path)
         } else {
             Ok(solc_path)
