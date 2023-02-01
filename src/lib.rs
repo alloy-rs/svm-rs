e4f07900c269b65bace932490c8544aa000505c4
Rohit Narurkar <rohit.narurkar@protonmail.com>
2023-02-01 23:53:43 +0800

    chore: v0.8.18 support (apple silicon)

--- src/lib.rs
+++ src/lib.rs
@@ -493,7 +493,7 @@ mod tests {
     // ensures we can download the latest native solc for apple silicon
     #[tokio::test(flavor = "multi_thread")]
     async fn can_download_latest_native_apple_silicon() {
-        let latest: Version = "0.8.15".parse().unwrap();
+        let latest: Version = "0.8.18".parse().unwrap();
 
         let artifacts = all_releases(Platform::MacOsAarch64).await.unwrap();
 
