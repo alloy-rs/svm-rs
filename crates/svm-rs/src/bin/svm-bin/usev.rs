e95721d418eabce1710434b323f279c09af7f24e
Enrique Ortiz <hi@enriqueortiz.dev>
2023-12-14 13:17:14 -0400

    properly handle uninstalled, existing versions in use

--- crates/svm-rs/src/bin/svm-bin/usev.rs
+++ crates/svm-rs/src/bin/svm-bin/usev.rs
@@ -15,6 +15,7 @@ impl UseArgs {
         let version = Version::parse(&self.version)?;
         let all_versions = svm_lib::all_versions().await?;
         let installed_versions = svm_lib::installed_versions().unwrap_or_default();
+        let current_version = svm_lib::current_version()?;
 
         if installed_versions.contains(&version) {
             svm_lib::use_version(&version)?;
@@ -27,11 +28,13 @@ impl UseArgs {
                 .default("N".into())
                 .interact_text()?;
             if matches!(input.as_str(), "y" | "Y" | "yes" | "Yes") {
-                // TODO: Break up install fn into minimal install
-                crate::install::InstallArgs::run(InstallArgs {
-                    versions: vec![self.version],
-                })
-                .await?;
+                let spinner = print::installing_version(&version);
+                svm_lib::install(&version).await?;
+                spinner.finish_with_message(format!("Downloaded Solc: {version}"));
+                if current_version.is_none() {
+                    svm_lib::use_version(&version)?;
+                    print::set_global_version(&version);
+                }
             }
         } else {
             print::unsupported_version(&version);
