6961d94bcde69630b2d2d839462adb1021d2f725
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-05-07 22:50:15 +0200

    feat: 0.8.30

--- crates/svm-rs/src/install.rs
+++ crates/svm-rs/src/install.rs
@@ -307,7 +307,7 @@ mod tests {
     use rand::seq::IndexedRandom;
 
     #[allow(unused)]
-    const LATEST: Version = Version::new(0, 8, 29);
+    const LATEST: Version = Version::new(0, 8, 30);
 
     #[tokio::test]
     #[serial_test::serial]
