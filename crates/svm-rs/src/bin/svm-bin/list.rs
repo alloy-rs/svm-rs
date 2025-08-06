c95a10d35f16a0eee78b5300ad1c15de55b686cd
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-08-07 00:55:54 +0200

    feat: make svm list work offline (#164)
    Print only what's been able to be fetched.

--- crates/svm-rs/src/bin/svm-bin/list.rs
+++ crates/svm-rs/src/bin/svm-bin/list.rs
@@ -10,20 +10,43 @@ pub struct ListCmd;
 
 impl ListCmd {
     pub async fn run(self) -> anyhow::Result<()> {
-        let all_versions = svm::all_versions().await?;
-        let installed_versions = svm::installed_versions().unwrap_or_default();
-        let current_version = svm::get_global_version()?;
+        let mut failed = false;
+        let mut err = |e: &svm::SvmError, s: &str| {
+            failed = true;
+            eprintln!("{s}: {e}");
+        };
 
-        let a: HashSet<Version> = all_versions.iter().cloned().collect();
-        let b: HashSet<Version> = installed_versions.iter().cloned().collect();
-        let c = &a - &b;
+        let all_versions = svm::all_versions()
+            .await
+            .inspect_err(|e| err(e, "Error fetching all versions"))
+            .unwrap_or_default();
+        let installed_versions = svm::installed_versions()
+            .inspect_err(|e| err(e, "Error fetching installed versions"))
+            .unwrap_or_default();
+        let current_version =
+            svm::get_global_version().inspect_err(|e| err(e, "Error fetching current version"));
 
-        let mut available_versions = c.iter().cloned().collect::<Vec<Version>>();
+        let mut available_versions = {
+            let a: HashSet<Version> = all_versions.iter().cloned().collect();
+            let b: HashSet<Version> = installed_versions.iter().cloned().collect();
+            let c = &a - &b;
+            c.iter().cloned().collect::<Vec<Version>>()
+        };
         available_versions.sort();
 
-        print::current_version(current_version);
-        print::installed_versions(installed_versions);
-        print::available_versions(available_versions);
+        if let Ok(current_version) = current_version {
+            print::current_version(current_version);
+        }
+        if !installed_versions.is_empty() {
+            print::installed_versions(installed_versions);
+        }
+        if !available_versions.is_empty() {
+            print::available_versions(available_versions);
+        }
+
+        if failed {
+            std::process::exit(1);
+        }
 
         Ok(())
     }
